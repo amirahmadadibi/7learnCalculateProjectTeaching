44ac132974a8d47d4139f5b24bf89ceb16122797
amirahmadadibi <amirahmadadibi@gmail.com>
2023-03-27 18:32:41 +0330

    s03 flutter - E05 use font and textstyle

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,17 @@ class MyWidget extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Container(
-            width: 300,
-            height: 300,
-            color: Colors.green,
-            child: Center(
-              child: Image(
-                  image: NetworkImage(
-                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg')),
+            child: Container(
+          width: 300,
+          height: 300,
+          child: Center(
+            child: Text(
+              'سون لرن',
+              style: TextStyle(
+                  fontSize: 38, fontFamily: 'vazir', color: Colors.blue),
             ),
           ),
-        ),
+        )),
       ),
     );
   }
